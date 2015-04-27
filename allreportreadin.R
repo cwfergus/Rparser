d80266d097cfbe4d23f71dca895f050ec5c43cb5
Cameron Ferguson <cameron.w.ferguson@gmail.com>
2015-04-27 11:19:53 -0700

    Modified it to work better

--- allreportreadin.R
+++ allreportreadin.R
@@ -1,20 +1,27 @@
 #all report read in
 
-filenames <- list.files("Reportfiles", pattern="*.rpt", full.names=TRUE)
+filenames <- list.files("Y:/", pattern="*.rpt", full.names=TRUE)
 # fix this again...
-filestoreadin <- filenames[!filenames %in% readinfiles]
+filestoreadin <- character()
+if (length(filestoreadin) == 0) {
+        filestoreadin <- filenames
+} else {
+        filestoreadin <- filenames[!filenames %in% readinfiles]
+}
+
+
 
 if (length(filestoreadin) == 0){
         print("No new report files to read in")
 }
 
 data <- ldply(lapply(filestoreadin, scan, 
-       what= list(Type="", Value = ""),
-       sep = "\t",
-       multi.line = TRUE,
-       fill=TRUE,
-       comment.char = "{",
-       allowEscapes = FALSE), as.data.frame, stringsAsFactors=FALSE) 
+                     what= list(Type="", Value = ""),
+                     sep = "\t",
+                     multi.line = TRUE,
+                     fill=TRUE,
+                     comment.char = "{",
+                     allowEscapes = FALSE), as.data.frame, stringsAsFactors=FALSE) 
 
 
 readinfiles <- filenames
