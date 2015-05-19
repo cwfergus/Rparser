418436605c6e21f97a0d98bc61f2f7a0594d2308
Cameron Ferguson <cameron.w.ferguson@gmail.com>
2015-05-19 14:36:27 -0700

    Curvecolor functions seems to be working

--- reportparserwimages.R
+++ reportparserwimages.R
@@ -286,16 +286,21 @@ coa_plot <- function(x,y,x_txt,y_txt,l_txt,pt_b,pt_c,fb,gr_t,labels,l_color="bla
           "//Data/it/DBMS/Integrations/ERPDev/imgs/lcms/" -> md
           dev_fn <- paste(md, fb, "_", gr_t, ".png", sep="")
           png(filename=dev_fn, width=1400, height=500)        # Open a device: bmp & pdf are other options here
-          plot(	x, y, 
-                main=labels[1], sub=labels[2],xlab=labels[6], ylab=labels[7], type="n", axes=TRUE 
-          )
-          if(pt_b) textxy(x_txt, y_txt, l_txt, cex = 1)
-          lines(x,y, lty=1, type=labels[8], pch='', col="red")
-          
-          text(max(x),max(y), labels[3], pos=2)
-          text(max(x),0.96*max(y), labels[4], pos=2)
-          text(max(x),0.92*max(y), labels[5], pos=2)
+        plot(x, y, 
+             main=labels[1], sub=labels[2],xlab=labels[6], ylab=labels[7], type="n", axes=TRUE,
+             col.lab="blue", col.axis="blue"
+        )
+        if(pt_b) textxy(x_txt, y_txt, l_txt, cex = 1, offset=-0.8)
+        if(pt_c) {
+                textxy(x_txt, y_txt-10, l_txt, cex=1, offset=-0.8)
+                polygon(shd, density=NA, col="green", border="black")
+                text (min(x), max(y), labels[9], pos=4)
+        }
+        lines(x,y, lty=1, type=labels[8], pch='', col=l_color)
         
+        text(max(x),max(y), labels[3], pos=2)
+        text(max(x),0.96*max(y), labels[4], pos=2)
+        text(max(x),0.92*max(y), labels[5], pos=2)
           dev.off()
         
         
@@ -313,6 +318,7 @@ coa_plot <- function(x,y,x_txt,y_txt,l_txt,pt_b,pt_c,fb,gr_t,labels,l_color="bla
         if(pt_c) {
                 textxy(x_txt, y_txt-10, l_txt, cex=1, offset=-0.8)
                 polygon(shd, density=NA, col="green", border="black")
+                text (min(x), max(y), labels[9], pos=4)
         }
         lines(x,y, lty=1, type=labels[8], pch='', col=l_color)
         
@@ -322,6 +328,37 @@ coa_plot <- function(x,y,x_txt,y_txt,l_txt,pt_b,pt_c,fb,gr_t,labels,l_color="bla
         dev.off()
         # Close the image device that was opened above; the actions in the meantime have been recorded
         return(fn)
+}
+
+curvecolor <- function(ploc, tdf, df) {
+        
+        
+        shd_bl <- grep(df[ploc+4, 2], tdf[,1], fixed=TRUE)
+        shd_el <- grep(df[ploc+5,1], tdf[,1], fixed=TRUE)
+        if (length(shd_bl) == 0) {
+                bt_m1 <- type.convert(df[ploc+4,2])-0.0001
+                shd_bl_m1 <- grep(bt_m1, tdf[,1], fixed=TRUE)
+                bt_p1 <- type.convert(df[ploc+4,2])+0.0001
+                shd_bl_p1 <- grep(bt_p1, tdf[,1], fixed=TRUE)
+                if (length(shd_bl_m1) > 0) shd_bl <- shd_bl_m1
+                if (length(shd_bl_p1) > 0) shd_bl <- shd_bl_p1
+        }
+        if (length(shd_el) == 0) {
+                et_m1 <- type.convert(df[ploc+5,1])-0.0001
+                shd_el_m1 <- grep(et_m1, tdf[,1], fixed=TRUE)
+                et_p1 <- type.convert(df[ploc+5,1])+0.0001
+                shd_el_p1 <- grep(et_p1, tdf[,1], fixed=TRUE)
+                if (length(shd_el_m1) > 0) shd_el <- shd_el_m1
+                if (length(shd_el_p1) > 0) shd_el <- shd_el_p1
+                
+        }
+        
+        shd_b <- min(shd_bl)
+        shd_e <- max(shd_el)
+        shd <- tdf[shd_b:shd_e,]
+        shd[1,2] <- 0
+        shd[length(shd[,1]),2] <- 0
+        return (shd)
 }
 
 #####Graphing Parser####
@@ -372,15 +409,20 @@ for (i in 1:imax)
                         lc_peak_loc <- grep("[PEAK]", lc_data[,1], fixed=TRUE)
                         lc_areatable <- data.frame()
                         for (lc in 1:length(lc_peak_loc)) {
+                                lc_pl <- lc_peak_loc[lc]
                                 lc_rt <- type.convert(lc_data[lc_peak_loc[lc]+3,2])
                                 lc_area <- type.convert(lc_data[lc_peak_loc[lc]+11,2])
-                                lc_table <- cbind(lc_rt, lc_area)
+                                lc_table <- cbind(lc_rt, lc_area, lc_pl)
                                 lc_areatable <- rbind(lc_areatable, lc_table)
                         }
                         peak_lc <- lc_areatable[which(lc_areatable$lc_area==max(lc_areatable$lc_area)),]
                         x_peak_lc <- peak_lc$lc_rt
                         y_peak_lc <- peak_lc$lc_area
+                        lc_peak <- peak_lc$lc_pl
                         y_peak_txt_lc <- paste("Area % Purity:", y_peak_lc, "%")
+                        
+                        lc_shd <- curvecolor(lc_peak, t_lci_LC, lc_data)
+                        
                         
                         #Analyze FLR data
                         m_lci_flr <- g_spi[min(which(g_spi>g_lci[2]))]-1
@@ -404,11 +446,14 @@ for (i in 1:imax)
                         flr_peak <- peak_flr$flr_pl
                         y_peak_txt_flr <- paste("Fluoresent % Purity:", y_peak_flr, "%")
                         
-                        flr_shd_b <- min(grep(flr_data[flr_peak+4,2], t_lci_flr[,1], fixed=TRUE))
-                        flr_shd_e <- max(grep(flr_data[flr_peak+5,1], t_lci_flr[,1], fixed=TRUE))
-                        flr_shd <- t_lci_flr[flr_shd_b:flr_shd_e,]
-                        flr_shd[1,2] <- 0
-                        flr_shd[length(flr_shd[,1]),2] <- 0
+                        flr_shd <- curvecolor(flr_peak, t_lci_flr, flr_data)
+                        
+                        #Find Range Values
+                range <- type.convert(txti[grep("MaxIntensity", txti[,1]),2])
+                lc_dis_range <- paste("Range:", round(range[1]/1000000, digits=3))
+                flr_dis_range <- paste("Range:", range[2])
+                
+                
                 } else {
                 # analyze hplc data
                 m_lci <- g_spi[min(which(g_spi>g_lci))]-1	# get the lowest position of } that's after [TRACE]
@@ -432,37 +477,13 @@ for (i in 1:imax)
                 y_peak_txt <- paste("Area % Purity:", y_peak, "%")
                 
                 #Generate Peak integration graphing data
-                roundmessage <- "I didn't round any peaks"
-                shd_bl <- grep(txti[peakloc+4,2], t_lci[,1], fixed=TRUE)
-                shd_el <- grep(txti[peakloc+5,1], t_lci[,1], fixed=TRUE)
-                if (length(shd_bl) == 0) {
-                        bt_m1 <- type.convert(txti[peakloc+4,2])-0.0001
-                        shd_bl_m1 <- grep(bt_m1, t_lci[,1], fixed=TRUE)
-                        bt_p1 <- type.convert(txti[peakloc+4,2])+0.0001
-                        shd_bl_p1 <- grep(bt_p1, t_lci[,1], fixed=TRUE)
-                        if (length(shd_bl_m1) > 0) shd_bl <- shd_bl_m1
-                        if (length(shd_bl_p1) > 0) shd_bl <- shd_bl_p1
-                        roundmessage <- "I had to round the leading shoulder"
-                }
-                if (length(shd_el) == 0) {
-                        et_m1 <- type.convert(txti[peakloc+5,1])-0.0001
-                        shd_el_m1 <- grep(et_m1, t_lci[,1], fixed=TRUE)
-                        et_p1 <- type.convert(txti[peakloc+5,1])+0.0001
-                        shd_el_p1 <- grep(bt_p1, t_lci[,1], fixed=TRUE)
-                        if (length(shd_el_m1) > 0) shd_el <- shd_el_m1
-                        if (length(shd_el_p1) > 0) shd_el <- shd_el_p1
-                        roundmessage <- "I had to round the trailing shoulder"
-                }
-                
-                shd_b <- min(shd_bl)
-                shd_e <- max(shd_el)
-                shd <- t_lci[shd_b:shd_e,]
-                shd[1,2] <- 0
-                shd[length(shd[,1]),2] <- 0
-                }
+                lc_shd <- curvecolor(peakloc, t_lci, txti)
                 #Find LC range value
                 range <- type.convert(txti[grep("MaxIntensity", txti[,1]),2])
-                dis_range <- paste("Range:", round(range/1000000, digits=3))
+                lc_dis_range <- paste("Range:", round(range/1000000, digits=3))
+                }
+                
+                
                 # analyze sample header data
                 t_hdr <- txti[1:g_mzi,]				# everything before mass spec data is header text
                 dfb   <- ProdNum[i]	# Finds prodNum
@@ -484,57 +505,28 @@ for (i in 1:imax)
                 buf_col <- t_hdr[grep("^Conditions", t_hdr[,1]),2]
                 lcl_2 <- buf_col  # hplc conditions
                 lcl_3 <- ""  # hplc conditions
-                lclabs  <- c(paste("HPLC:",dfb),"",lcl_1,lcl_2,lcl_3,"Time","Absorbance %","o", dis_range)
+                lclabs  <- c(paste("HPLC:",dfb),"",lcl_1,lcl_2,lcl_3,"Time","Absorbance %","o", lc_dis_range)
                 
                 fn_mz <- coa_plot(x_mzi,y_mzi,x_txt,y_txt,x_txt,TRUE,FALSE,dfb,"mz",mzlabs, l_col="red", so=SO[i])        # mass spec
                 
                 if (length(g_lci) == 1) {
-                fn_lc <- coa_plot(x_lci,y_lci,x_peak,y_peak,y_peak_txt,FALSE,TRUE,dfb,"lc",lclabs, shd=shd, so=SO[i])	# hplc
+                fn_lc <- coa_plot(x_lci,y_lci,x_peak,y_peak,y_peak_txt,FALSE,TRUE,dfb,"lc",lclabs, shd=lc_shd, so=SO[i])	# hplc
                 } else {
-                        fn_lc <- coa_plot(x_lci_LC, y_lci_LC, x_peak_lc, y_peak_lc, y_peak_txt_lc,FALSE, TRUE, dfb, "lc", lclabs, so=SO[i])
+                        fn_lc <- coa_plot(x_lci_LC, y_lci_LC, x_peak_lc, y_peak_lc, y_peak_txt_lc,FALSE, TRUE, dfb, "lc", lclabs, shd=lc_shd, so=SO[i])
                         
                         flrl_1 <- grep("ACQUITY FLR", txti[,2], value=TRUE)
                         flrl_2 <- buf_col
                         flrl_3 <- ""
-                        flrlabs <- c(paste("FLR", dfb),"",flrl_1, flrl_2, flrl_3, "Time", "Emission %", "o")
+                        flrlabs <- c(paste("FLR", dfb),"",flrl_1, flrl_2, flrl_3, "Time", "Emission %", "o", flr_dis_range)
                         
                         fn_flr <- coa_plot(x_lci_flr, y_lci_flr, x_peak_flr, y_peak_flr, y_peak_txt_flr,FALSE,TRUE,dfb,"flr",flrlabs, shd=flr_shd, so=SO[i])
                 }
-                sample <- paste(fn_mz, fn_lc, roundmessage, sep="\t")
-                fn_list <- paste(fn_list, sample, sep='\n')
+                
                 
         }
 }
-write.table(fn_list, "processedlog.txt")
+
 
 #rm(list=ls())
 
 ##### testing####
-curvecolor <- function(ploc, tdf) {
-        shd_bl <- grep(txti[ploc+4, 2], tdf[,1], fixed=TRUE)
-        shd_el <- grep(txti[ploc+5,1], tdf[,1], fixed=TRUE)
-        if (length(shd_bl) == 0) {
-                bt_m1 <- type.convert(txti[ploc+4,2])-0.0001
-                shd_bl_m1 <- grep(bt_m1, tdf[,1], fixed=TRUE)
-                bt_p1 <- type.convert(txti[ploc+4,2])+0.0001
-                shd_bl_p1 <- grep(bt_p1, tdf[,1], fixed=TRUE)
-                if (length(shd_bl_m1) > 0) shd_bl <- shd_bl_m1
-                if (length(shd_bl_p1) > 0) shd_bl <- shd_bl_p1
-        }
-        if (length(shd_el) == 0) {
-                et_m1 <- type.convert(txti[ploc+5,1])-0.0001
-                shd_el_m1 <- grep(et_m1, tdf[,1], fixed=TRUE)
-                et_p1 <- type.convert(txti[ploc+5,1])+0.0001
-                shd_el_p1 <- grep(et_p1, tdf[,1], fixed=TRUE)
-                if (length(shd_el_m1) > 0) shd_el <- shd_el_m1
-                if (length(shd_el_p1) > 0) shd_el <- shd_el_p1
-
-        }
-        
-        shd_b <- min(shd_bl)
-        shd_e <- max(shd_el)
-        shd <- tdf[shd_b:shd_e,]
-        shd[1,2] <- 0
-        shd[length(shd[,1]),2] <- 0
-        return (shd)
-}
