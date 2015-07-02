b28e39fb2f5ebd1f5d0870d0dce29b796e5d5884
Cameron Ferguson <cameron.w.ferguson@gmail.com>
2015-07-02 12:53:51 -0700

    Updated Custom labeler to be take on more responsiblities

--- customlabeler.R
+++ customlabeler.R
@@ -1,7 +1,9 @@
 
-custom.labels <- function (sl_x, sl_y, sl_labels = NULL, x_offsets = NA, offset_amt=15
-                                  linecol = par("fg"), srt = 0, ...) 
+custom.labels <- function (og_x, og_y, og_labels = NULL, x_offsets = NA, ofs_amt=15,
+                           linecol = par("fg"), srt = 0, ...) 
 {
+        
+        
         rounder <- function(x) {round(x+10^-9)}
         
         posfinder <- function(x) {
@@ -15,41 +17,60 @@ custom.labels <- function (sl_x, sl_y, sl_labels = NULL, x_offsets = NA, offset_
                 x
         }
         
-        ny <- length(sl_y)
-        divide <- rounder(ny/2)
-        
-        if (is.na(x_offsets)) {
-                x_offset <- diff(par("usr")[1:2])/offset_amt
-                x_offsets <- rep(c(rep(-x_offset, divide), rep(x_offset, divide)), ny/2 + 1)[1:ny]
+        limit_finder <- function(og_y) {x <- seq(0, 1, .05)
+                                    
+                                    for (i in 1:length(x)) {
+                                            threshold <- x[i]
+                                            min <- threshold*max(og_y)
+                                            y_min <- which(og_y>min)
+                                            if (length(y_min)<=10) {
+                                                    threshold
+                                                    break
+                                            }
+                                    }
+                                    threshold
         }
         
-        new_x <- sl_x + x_offsets
-        
-        x_left_pts <- new_x[1:divide]
-        left_labels <- sl_labels[1:divide]
-        
-        x_right_pts <- new_x[(divide+1):length(new_x)]
-        right_labels <- sl_labels[(divide+1):length(new_x)]
-        
-        
-        
-        y_left_pts <- sl_y[1:divide]
-        y_right_pts <- sl_y[(divide+1):length(sl_y)]
-        
-        y_left_pts <- posfinder(y_left_pts)
-        y_right_pts <- posfinder(y_right_pts)
-        
-        new_y <- c(y_left_pts, y_right_pts)
-        #                         sl_x <- sl_x + x_offsets
-        #                         sort.index <- sort.list(sl_x)
-        #                         sl_x <- sl_x[sort.index]
-        #                         sl_y <- sl_y[sort.index]
-        #                         nx <- length(sl_x)
-        #                         newx <- seq(sl_x[1], sl_x[nx], length = length(sl_labels))
-        
-        segments(new_x, new_y, sl_x, sl_y)
-        
-        text(x_left_pts, y_left_pts, left_labels, srt = srt, pos=2)
-        text(x_right_pts, y_right_pts, right_labels, srt=srt, pos=4)
+        ny <- length(og_y)
+        if (ny == 1) {
+                text(og_x, og_y, og_labels, pos=3)
+        } else {
+                
+                limit <- limit_finder(og_y)
+                min <- limit*max(og_y)
+                y_min <- which(og_y>min)
+                og_x <- og_x[y_min]
+                og_y <- og_y[y_min]
+                og_labels <- og_labels[y_min]
+                ny <- length(og_y)
+                divide <- rounder(ny/2)
+                
+                if (is.na(x_offsets)) {
+                        x_offset <- diff(par("usr")[1:2])/ofs_amt
+                        x_offsets <- rep(c(rep(-x_offset, divide), rep(x_offset, divide)), ny/2 + 1)[1:ny]
+                }
+                
+                new_x <- og_x + x_offsets
+                
+                x_left_pts <- new_x[1:divide]
+                left_labels <- og_labels[1:divide]
+                
+                x_right_pts <- new_x[(divide+1):length(new_x)]
+                right_labels <- og_labels[(divide+1):length(new_x)]
+                
+                
+                
+                y_left_pts <- og_y[1:divide]
+                y_right_pts <- og_y[(divide+1):length(og_y)]
+                
+                y_left_pts <- posfinder(y_left_pts)
+                y_right_pts <- posfinder(y_right_pts)
+                
+                new_y <- c(y_left_pts, y_right_pts)
+                
+                segments(new_x, new_y, og_x, og_y)
+                text(x_left_pts, y_left_pts, left_labels, srt = srt, pos=2)
+                text(x_right_pts, y_right_pts, right_labels, srt=srt, pos=4)
+        }
         
 }
